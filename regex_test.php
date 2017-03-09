8a2a8ab4b812ad9f75e91f3a73972f61599c1f8a
SmartGuy <adam.lindberg520@gmx.com>
2017-03-09 14:21:12 +0100

    logic updated to display all people

--- regex_test.php
+++ regex_test.php
@@ -1,13 +1,21 @@
 <?php
     $raw_content = file_get_contents("./results_raw.txt");
-    $people_id = "19373";
-    $pattern = "/<TR>(.+)<A (.+)PeopleID=" . $people_id . "(.)+>(.+)<\/A>(.)+<\/TD>\R<TD nowrap valign=top>(((.)+)\&nbsp\;)<\/TD>\R<TD valign=top><A HREF=Results\.asp\?Institution=/";
+//    $people_id = "19373";
+//    $pattern = "/<TR>(.+)<A (.+)PeopleID=" . $people_id . "(.)+>(.+)<\/A>(.)+<\/TD>\R<TD nowrap valign=top>(((.)+)\&nbsp\;)<\/TD>\R<TD valign=top><A HREF=Results\.asp\?Institution=/";
+    $pattern = "/<TR>(.+)<A (.+)PeopleID=((\d)+) onclick=(.)+>(.+)<\/A>(.)+<\/TD>\R<TD nowrap valign=top>(((.)+)\&nbsp\;)<\/TD>\R<TD valign=top><A HREF=Results\.asp\?Institution=/";
     preg_match_all($pattern, $raw_content, $matches, PREG_OFFSET_CAPTURE);
-//    print_r($matches);
-    echo 'lastname: ' . $matches[4][0][0];
-    echo 'fist and middle name: ' . $matches[6][0][0];
-    $output_file = fopen($people_id . '_result.csv', 'w');
+    print_r($matches);
+//    echo 'peopleID: ';
+//    echo 'lastname: ' . $matches[6][0][0];
+//    echo 'fist and middle name: ' . $matches[9][0][0];
+
+    $output_file = fopen('result.csv', 'w');
     fputcsv($output_file, array('peopleID', 'first/middle name', 'lastname'));
-    fputcsv($output_file, array($people_id, $matches[7][0][0], $matches[4][0][0]));
+    
+    for ($i = 0; $i <= count($matches[3]); $i++) {
+        fputcsv($output_file, array($matches[3][$i][0], $matches[9][$i][0], $matches[6][$i][0]));
+    }
+    
+//    fputcsv($output_file, array($people_id, $matches[9][0][0], $matches[6][0][0]));
     fclose($output_file);
 ?>
